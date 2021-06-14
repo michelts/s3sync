6fdb595184539ee0d3e9467bfb7ec06508826a4e
Michel Sabchuk <michel@turbosys.com.br>
2021-06-14 10:16:09 -0300

    Add some kind of verbosity

--- test.py
+++ test.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
 import csv
 import requests
+import time
 
 
 def main():
+    print('Starting')
     for publisher, publication, issue in csv.reader(open("items.csv")):
-        print('Parsing {}/{}/{}'.format(publisher, publication, issue))
+        t0 = time.time()
         requests.post(
             "http://localhost:5000/",
             json={
@@ -14,6 +16,8 @@ def main():
                 "Issue": int(issue),
             },
         )
+        delta = time.time() - t0
+        print('Parsed {}/{}/{} in {} seconds'.format(publisher, publication, issue, delta))
 
 
 if __name__ == "__main__":
